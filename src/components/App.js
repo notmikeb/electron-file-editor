a25f246f8d9f43acc4b49fe676d3109b509873f6
francesco-strazzullo <me@francescostrazzullo.info>
2016-05-02 21:26:48 +0200

    Added FAB

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,13 @@
 import AppBar from 'material-ui/lib/app-bar';
+import FloatingActionButton from 'material-ui/lib/floating-action-button';
+import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import React from 'react';
+
+const style = {
+  position: "fixed",
+  bottom: "20px",
+  right: "20px"
+};
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +16,9 @@ export default class App extends React.Component {
             <AppBar
               title="electron-file-editor"
               showMenuIconButton={false}/>
+          <FloatingActionButton style={style}>
+               <ContentAdd />
+             </FloatingActionButton>
         </div>
       );
   }
