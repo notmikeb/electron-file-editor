34cdde0328d0d11b1fa3936c3c8d2f4ad93b8e39
francesco-strazzullo <me@francescostrazzullo.info>
2016-06-23 15:34:17 +0200

    cosmetics

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
-import AppBar from 'material-ui/AppBar';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
-import {List, ListItem} from 'material-ui/List';
-import EditIcon from 'material-ui/svg-icons/editor/mode-edit';
 import React from 'react';
-import Files from '../model/Files';
 
+import AppBar from 'material-ui/AppBar';
 import FileList from './FileList';
 import Editor from './Editor';
+
+import Files from '../model/Files';
 
 export default class App extends React.Component {
     constructor(props) {
